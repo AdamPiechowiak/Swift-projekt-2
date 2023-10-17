3608cedb8ab9399041023da4ae384ff3055fb6d2
nobody <nobody>
2023-10-17 10:17:36 +0200

    polecenie 1

---
+++ MemoryGame/MemoryGame/CardView.swift
@@ -0,0 +1,46 @@
+//
+//  CardView.swift
+//  MemoryGame
+//
+//  Created by student on 17.10.2023.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    
+    @State var isFaceUp:Bool = true
+    
+    var body: some View {
+        Group{
+            if(isFaceUp)
+            {
+                RoundedRectangle(cornerRadius: 12)
+                    .stroke(Color(.blue), lineWidth: 2)
+                    .frame(width: 100.0, height: 100.0)
+                
+                    .overlay(
+                        Text("🎃").font(.largeTitle)
+                    )
+            }
+            else
+            {
+                RoundedRectangle(cornerRadius: 12)
+                    .fill(.blue)
+                    .frame(width: 100.0, height: 100.0)
+                    
+                
+                
+            }
+        }.onTapGesture {
+            isFaceUp.toggle()
+        }
+        
+            
+            
+    }
+}
+
+#Preview {
+    CardView()
+}
